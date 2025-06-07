2e11a78d2f3a872493919e33fa7b4a0786ec6fd5
MarinaKorn <gecagem840@gmail.com>
2025-06-07 22:32:07 +0200

    4

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,5 +1,4 @@
-import { useState } from "react";
-import { toast } from "react-hot-toast";
+import toast from "react-hot-toast";
 import css from "./SearchBar.module.css";
 
 interface SearchBarProps {
@@ -7,10 +6,8 @@ interface SearchBarProps {
 }
 
 export default function SearchBar({ onSubmit }: SearchBarProps) {
-  const [searchTerm, setSearchTerm] = useState("");
-
-  async function formAction(formData: FormData) {
-    const query = formData.get("query")?.toString().trim() || "";
+  const handleSubmit = (formData: FormData) => {
+    const query = (formData.get("query") as string).trim();
 
     if (!query) {
       toast.error("Please enter your search query.");
@@ -18,36 +15,20 @@ export default function SearchBar({ onSubmit }: SearchBarProps) {
     }
 
     onSubmit(query);
-    setSearchTerm(""); 
-  }
+  };
 
   return (
-    <header className={css.header}>
-      <div className={css.container}>
-        <a
-          className={css.link}
-          href="https://www.themoviedb.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by TMDB
-        </a>
-        <form className={css.form} action={formAction}>
-          <input
-            className={css.input}
-            type="text"
-            name="query"
-            autoComplete="off"
-            placeholder="Search movies..."
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-            autoFocus
-          />
-          <button className={css.button} type="submit">
-            Search
-          </button>
-        </form>
-      </div>
-    </header>
+    <form action={handleSubmit} className={css.form}>
+      <input
+        type="text"
+        name="query"
+        autoComplete="off"
+        placeholder="Search movies..."
+        className={css.input}
+      />
+      <button type="submit" className={css.button}>
+        Search
+      </button>
+    </form>
   );
 }
